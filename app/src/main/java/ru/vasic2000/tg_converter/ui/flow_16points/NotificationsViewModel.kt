37f8ece7e3cff4e7127867c9ed48dce8e506f9fc
Vasic2000 <vasic2000@yandex.ru>
2025-02-16 17:23:56 +0300

    Init commit

---
+++ app/src/main/java/ru/vasic2000/tg_converter/ui/flow_16points/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ru.vasic2000.tg_converter.ui.flow_16points
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
